79415c4f249daa8e5529e36d06ac0cc1ad144207
alinaw89 <ali.naw89@gmail.com>
2015-07-11 17:50:08 -0400

    adds getUsers and getUser factory functions

--- app/services/usersFactory.js
+++ app/services/usersFactory.js
@@ -7,8 +7,20 @@
     factory.getUsers = function() {
       return $http.get('http://localhost:3000/users').success(function(response) {
         angular.copy(response, factory.users);
-      })
-    }
-  }
+      });
+    };
+
+    factory.getUser = function(userId) {
+      return $http.get('http://localhost:3000/users/' + userId).success(function(response) {
+        angular.copy(response, factory.user);
+      });
+    };
+
+    return factory;
+  };
+
+  usersFactory.$inject = ['$http'];
+
+  angular.module('usersApp').factory('usersFactory', usersFactory);
 
 })();
