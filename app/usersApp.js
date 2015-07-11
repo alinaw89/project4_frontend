c1ef44fec5bcc9285368e636bd2563694be31342
alinaw89 <ali.naw89@gmail.com>
2015-07-11 17:56:12 -0400

    adds userApp route config

--- app/usersApp.js
+++ app/usersApp.js
@@ -1,1 +1,15 @@
-angular.module('usersApp', []);
+(function usersAppIIFE() {
+  var app = angular.module('usersApp', ['ngRoute']);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        controller: 'usersController',
+        controllerAs: 'usersCtrl',
+        templateUrl: 'app/views/users.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+})();
