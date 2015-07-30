a3e218c5fe27a84ed9f416f721839da847e2045c
alinaw89 <ali.naw89@gmail.com>
2015-07-30 15:12:10 -0400

    fixes httpprovider to http

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('briefApp', ['ngRoute', 'appRoutes']);
 
 app.run(function($window, $http) {
-  $httpProvider.defaults.useXDomain = true;
-  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  $http.defaults.useXDomain = true;
+  delete $http.defaults.headers.common['X-Requested-With'];
   var token = $window.localStorage.getItem('gl-user-token');
   if (token) {
     $http.defaults.headers.common.Authorization = 'Token token=' + token;
